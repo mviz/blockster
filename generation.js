7d05c99f0d05a47c49cfae58da711189753c8cde
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-10-01 22:53:10 -0400

    init_generation and generate_next_block

--- generation.js
+++ generation.js
@@ -1,6 +1,33 @@
 
+function init_generation() {
+	blocks = [];
+
+	num_blocks = 20;
+
+	for(int i = 0; i < num_blocks; i++) {
+		blocks[] = {'width' : (Math.random() * BLOCK_WIDTH_MAX) + BLOCK_WIDTH_MAX, 
+					'x' : Math.random() * canvas.width, 
+					'y' : Math.random() * canvas.height};
+
+	}
+
+	return blocks;
+}
+
+next_block_frame = 0;
+
+function generate_next_block() {
+	
+	if(frame >= next_block_frame) {
+
+		return {'width' : (Math.random() * BLOCK_WIDTH_MAX) + BLOCK_WIDTH_MAX, 
+					'x' : canvas.width + 100, 
+					'y' : Math.random() * canvas.height};
 
 
-function initGeneration() {
-	blocks = []
+		next_block_frame = Math.random() * 1 / FRAME_RATE;
+
+	} 
+
+	return null;
 }
--- world.js
+++ world.js
@@ -1,1 +1,8 @@
 //{'width' : 100, 'x' : 10, 'y' : 10}
+
+
+BLOCK_WIDTH_MAX = 100;
+BLOCK_WIDTH_MIN = 50;
+FRAME_RATE = 60;
+
+frame = 0;

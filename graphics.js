dfba2eb368f492342943302b172a5385b90b5a01
mfritze <mfritze@ualberta.ca>
2015-10-01 22:55:55 -0400

    Adding graphics functions

---
+++ graphics.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+var avatar_color = "#EBE395";
+var block_color = "#08576B";
+
+function draw() {
+    context.clearRect(0,0, canvas.width, canvas.height);
+    draw_avatar();
+    draw_bricks();
+}
+
+function draw_avatar() {
+
+}
+
+function draw_bricks(){
+
+}
--- world.js
+++ world.js
@@ -1,1 +1,6 @@
 //{'width' : 100, 'x' : 10, 'y' : 10}
+
+
+//var interval_ID = set_interval(draw, 8);
+
+var brickList = 
